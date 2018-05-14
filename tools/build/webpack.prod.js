6003a613c899d4dd17e70ab791e1161ce7cc9c9a
Amr Yousef <amr@ineqe.com>
2018-05-14 10:35:46 +0100

    Replace webpack with parallel-webpack
    - Added helper functions to get list of all functions directories located in a directory
    - Added helper function to map list of all functions directories located in a directory into Webpack entry object
    - Modified webpack config file to generate a list of configuration objects for each entry found
    - renamed function files to be "index.ts"

--- tools/build/webpack.prod.js
+++ tools/build/webpack.prod.js
@@ -2,52 +2,54 @@ const $ = require('./helpers');
 const uglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const copyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  target: 'node',
-  entry: {
-    'some-function': $.root('./src/some-function/some-function.ts')
-  },
-  output: {
-    path: $.root('dist'),
-    filename: '[name]/[name].js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'awesome-typescript-loader?declaration=false',
-        exclude: [/\.(spec|e2e)\.ts$/]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    modules: [
-      'node_modules',
-      'src'
-    ]
-  },
-  plugins: [
-    new uglifyJSPlugin({
-      uglifyOptions: {
-        ecma: 6
-      }
-    }),
-    new copyWebpackPlugin([
-      {
-        from: 'src/host.json',
-        to: 'host.json'
-      },
-      {
-        context: 'src',
-        from: '**/function.json',
-        to: ''
-      }
-    ])
-  ],
-  node: {
-    __filename: false,
-    __dirname: false,
-  }
-};
+module.exports = $.getEntries('src',['test','__tests__','__mocks__','shared'],'index.ts').map(
+    entry => {
+        return {
+            target: 'node',
+            entry: entry,
+            output: {
+              path: $.root('dist'),
+              filename: '[name]/index.js',
+              libraryTarget: 'commonjs2'
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.ts$/,
+                  use: 'awesome-typescript-loader?declaration=false',
+                  exclude: [/\.(test|spec|e2e)\.ts$/]
+                }
+              ]
+            },
+            resolve: {
+              extensions: ['.ts', '.js', '.json'],
+              modules: [
+                'node_modules',
+                'src'
+              ]
+            },
+            plugins: [
+              new uglifyJSPlugin({
+                uglifyOptions: {
+                  ecma: 6
+                }
+              }),
+              new copyWebpackPlugin([
+                {
+                  from: 'src/host.json',
+                  to: 'host.json'
+                },
+                {
+                  context: 'src',
+                  from: '**/function.json',
+                  to: ''
+                }
+              ])
+            ],
+            node: {
+              __filename: false,
+              __dirname: false,
+            }
+          }
+    }
+) ;
